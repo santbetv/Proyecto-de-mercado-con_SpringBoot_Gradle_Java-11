00d4df6a14f11835f2d0af4089614624f1dc1303
Santiago Betancur <santiago-betancur@hotmail.com>
2020-12-01 02:33:01 -0500

    Inicio de proyecto de procesamiento de mercado, con Java 11 y SpringBoot, paquetamiento gradel

---
+++ src/main/java/com/santbetv/developermercado/DeveloperMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.santbetv.developermercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperMercadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santbetv/developermercado/DeveloperMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santbetv.developermercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
